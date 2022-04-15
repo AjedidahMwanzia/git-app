9aee33e75d3ceff1d9b29645602276aaa637c30f
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-15 22:15:18 +0300

    add access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Accesstoken:'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'
 };
 
 /*
