91c01e7e37b61aedfe7752e7f7b777fa8c452772
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-15 22:23:47 +0300

    add user and repo classes

---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor(public firstName:string,public lastName:string,public userName:string, public id:number){
+        this.firstName=firstName
+        this.lastName = lastName
+        this.userName = userName
+        this.id =id
+    }
+}
