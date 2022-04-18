71d9e4c4bdfb638626026ad460f4652501670371
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-19 01:16:19 +0300

    add toutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubAppComponent } from './github-app/github-app.component';
+import { GithubReposComponent } from './github-app/github-repos/github-repos.component';
+import { GithubUserComponent } from './github-app/github-user/github-user.component';
 
 const routes: Routes = [
+ {path:"github-app",component:GithubAppComponent},
+ {path:"github-Users",component:GithubUserComponent},
+ {path:"github-Repos",component:GithubReposComponent}
 ];
 
 @NgModule({
