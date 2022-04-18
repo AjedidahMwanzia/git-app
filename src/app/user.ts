1760a3c14d023f5458bd454ac69982cd09cda56f
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-18 15:03:45 +0300

    update style of user profile display

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor(public name:string,public html_url:any,public userName:string,public blog:string,public location:string,public followers:number,public following:number,public bio:string, public id:number){
+    constructor(public name:string,public login:any,public userName:string,public blog:string,public location:string,public followers:number,public following:number,public bio:string, public id:number){
         this.name =name
         this.userName = userName
         this.id =id
