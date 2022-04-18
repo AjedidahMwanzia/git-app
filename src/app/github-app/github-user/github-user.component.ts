02678adf602208231bb39990b5ff06baff95f8ca
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-18 23:27:27 +0300

    style avatar

--- src/app/github-app/github-user/github-user.component.ts
+++ src/app/github-app/github-user/github-user.component.ts
@@ -22,7 +22,7 @@ export class GithubUserComponent implements OnInit {
  getUser(username:string):void{
  this.ApiService.getUser(username).then ((users:any)=>{
 
-this.user = new User(users.login,users.avatar_url,users.gravatar_id,users.name,users.blog,users.login,users.location,users.followers,users.following,users.bio,users.id)
+this.user = new User(users.name,users.login,users.avatar_url,users.blog,users.location,users.followers,users.following,users.bio,users.id)
  console.log(this.user)
 })
   }
@@ -31,11 +31,11 @@ this.user = new User(users.login,users.avatar_url,users.gravatar_id,users.name,u
     this.getUser(username)
     return false
   }
-  
+
      
  ngOnInit(): void {
 
-
+  console.log(typeof this.user)
  }
 
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,6 @@
 export class User {
-    constructor(public name:string,public login:any,public gravatar_id:any,public avatar_url:any,public userName:string,public blog:string,public location:string,public followers:number,public following:number,public bio:string, public id:number){
+    constructor(public name:string,public login:any,public avatar_url:any,public blog:string,public location:string,public followers:number,public following:number,public bio:string, public id:number){
         this.name =name
-        this.userName = userName
         this.id =id
         
     }
